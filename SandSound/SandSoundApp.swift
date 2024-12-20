484714a55e1bd0b90461f21875e6161387869224
Manar Alghamdi <manar.a.gh@hotmail.com>
2024-12-21 01:32:13 +0300

    Initial Commit

---
+++ SandSound/SandSoundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandSoundApp.swift
+//  SandSound
+//
+//  Created by Manar Alghmadi on 21/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct SandSoundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
