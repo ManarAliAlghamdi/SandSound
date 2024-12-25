818f268179101ad6c001a733063c8b219dbdebd9
Manar Alghamdi <manar.a.gh@hotmail.com>
2024-12-25 19:18:49 +0300

    Gif  Change add pop up for game over

---
+++ SandSound/App/Components/Styles.swift
@@ -0,0 +1,18 @@
+//
+//  Colors.swift
+//  SandSound
+//
+//  Created by Manar Alghmadi on 25/12/2024.
+//
+
+import SwiftUI
+
+struct Colors: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Colors()
+}
